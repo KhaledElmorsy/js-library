5d470f35ab5fb679a88fa9b1e78ffe0d41611bb0
kelmorsy <khaled.mg.a@gmail.com>
2022-05-23 08:27:08 +0200

    Add card DOM element to book object & methods

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
+const cardList = document.querySelector('.container');
+const cardTemplate = cardList.querySelector('.template')
 
-function Book(name, author, pages, read) {
-    this.name = new String(name);
-    this.author = new String(author);
-    this.pages = new String(pages);
-    this.read = read;
+
+function Book(name, author, pages) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+
+    this.card = cardTemplate.cloneNode(true);
+    this.card.querySelector('.name').innerText = name;
+    this.card.querySelector('.author').innerText = author;
+    this.card.querySelector('.pages').innerText = pages + " pages";
+    this.card.classList.toggle('template');
+    this.card.book = this;
+
+    cardList.appendChild(this.card);
 }
 
-Book.prototype.readToggle = function(){this.read = !this.read;}
+Book.prototype.readToggle = function(){
+    this.read = !this.read;
+    this.card.classList.toggle("read")}
+Book.prototype.removeCard = function(){
+    this.card.addEventListener('transitionend', (e)=>e.target.remove())
+    this.card.classList.add('remove');
+}
+
 
 function Library(){
     this.books = [];
 }
 
-Library.prototype.addBook = function(name, author, pages ,read){
-    this.books.push(new Book(name, author, pages ,read));
+Library.prototype.addBook = function(name, author, pages){
+    this.books.push(new Book(name, author, pages));
 }
-
-const books = document.querySelector('.container');
-const cardTemplate = books.querySelector('.template')
-
+Library.prototype.deleteBook = function(book){
+    book.removeCard();
+    this.books = this.books.filter((entry)=> entry != book)
+}
 
 const myLib = new Library();
 myLib.addBook('Lotr','JK',200,false);
-myLib.addBook('Harry', 'Jojo', 120, true)
-console.dir(myLib)
-cardTemplate
+myLib.addBook('Harry', 'Jojo', 120, true);
+myLib.addBook('Lotr','JK',200,false);
+console.dir(myLib);
 
 
-for(book of myLib.books){
-    const card = cardTemplate.cloneNode(true);
-    const cardName = card.querySelector('.name');
-    const cardAuthor = card.querySelector('.author');
-    const cardPages = card.querySelector('.pages');
-
-    cardName.innerText = book.name;
-    cardAuthor.innerText = book.author;
-    cardPages.innerText = book.pages + ' pages';
-    card.classList.toggle('template');
-
-    books.appendChild(card);
-}
-
-
-function newBook() {
-
-}
 
 const addBook = document.querySelector('.add-book');
